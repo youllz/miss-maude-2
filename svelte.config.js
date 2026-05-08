3c66feb8507d26377c8fb736e09dbdc28c32fbe1
youllz <youssoufzerbo01@gmail.com>
2026-05-08 21:39:25 +0000

    error page in build directory

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,15 @@ const config = {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
-	kit: { adapter: adapter() }
+	kit: { 
+		adapter: adapter({
+			pages: 'build',
+          assets: 'build',
+          fallback: '404.html', // Recommandé pour éviter les erreurs sur Sevalla
+          precompress: false,
+          strict: true
+		}),
+	 }
 };
 
 export default config;
